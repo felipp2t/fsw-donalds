87eeb4a969e86d5cc823589b868f45ad5e35ca1e
Felipe Rossetto <feliperossettodasilva123@gmail.com>
2025-02-21 15:02:32 -0300

    ✨ feat: validate consumption method in restaurant page

--- src/app/[slug]/menu/page.tsx
+++ src/app/[slug]/menu/page.tsx
@@ -1,13 +1,25 @@
+import { notFound } from "next/navigation";
+
 interface RestaurantPageProps {
-    params: Promise<{ slug: string }>;
+  params: Promise<{ slug: string }>;
+  searchParams: Promise<{ consumptionMethod: string }>;
 }
 
-export const RestaurantPage = async (props: RestaurantPageProps) => {
-    const { slug } = await props.params
-    
+const isConsumptionMethodValid = (consumptionMethod: string) => {
+  return ["DINE_IN", "TAKEAWAY"].includes(consumptionMethod.toUpperCase());
+};
+
+export default async function RestaurantPage(props: RestaurantPageProps) {
+  const { slug } = await props.params;
+  const { consumptionMethod } = await props.searchParams;
+
+  if (!isConsumptionMethodValid(consumptionMethod)) {
+    return notFound();
+  }
+
   return (
     <div>
       <h1>sdasd</h1>
     </div>
   );
-};
+}
