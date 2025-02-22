265617609d5d7d1aac15abea4bde3863faf588e9
Felipe Rossetto <feliperossettodasilva123@gmail.com>
2025-02-22 11:40:49 -0300

    ✨ feat: redirect to /fsw-donalds on page load

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,16 @@
-import { Button } from "@/components/ui/button";
+"use client";
+
+import { redirect } from "next/navigation";
+import { useEffect } from "react";
 
 export default function Home() {
+  useEffect(() => {
+    redirect("/fsw-donalds");
+  }, []);
+
   return (
     <main>
       <div>Hello world!</div>
-      <Button>Click me!</Button>
     </main>
   );
 }
