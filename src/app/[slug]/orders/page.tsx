8c41622d47b40ecd9a4664d48e8099ad7d8fccb4
Felipe Rossetto <feliperossettodasilva123@gmail.com>
2025-02-22 11:19:21 -0300

    🐛  fix: display order list on orders page

--- src/app/[slug]/orders/page.tsx
+++ src/app/[slug]/orders/page.tsx
@@ -1,6 +1,7 @@
 import { getOrdersByCpf } from "@/data/get-orders-by-cpf";
 import { isValidCpf } from "../menu/helpers/cpf";
 import { CpfForm } from "./components/cpf-form";
+import { OrderList } from "./components/order-list";
 
 interface OrdersPageProps {
   searchParams: Promise<{ cpf: string }>;
@@ -15,5 +16,5 @@ export const OrdersPage = async ({ searchParams }: OrdersPageProps) => {
 
   const { orders } = await getOrdersByCpf(cpf);
 
-  return <h1>adfad</h1>;
+  return <OrderList orders={orders} />;
 };
